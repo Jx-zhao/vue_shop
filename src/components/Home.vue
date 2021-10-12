6b5af2b8127383b81e401b4c9349078ec8281580
zhaojinxin <386608610@qq.com>
2021-10-12 16:32:24 +0800

    后台左侧导航完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,50 +1,111 @@
 <template>
   <el-container class="home-centainer">
+    <!-- 头部区域 -->
     <el-header>
-      <div><img src="../assets/home_logo.png" alt=""><span>电商后台管理系统</span></div>
-      <el-button type="info" @click="logout">退出</el-button></el-header>
+      <div>
+        <img src="../assets/home_logo.png" alt="" /><span
+          >电商后台管理系统</span
+        >
+      </div>
+      <el-button type="info" @click="logout">退出</el-button></el-header
+    >
     <el-container>
-      <el-aside width="200px">
-              <!-- default-active="2"
+      <el-aside :width="isCollapse?'64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- default-active="2"
       class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose" -->
-    <el-menu
-      background-color="#333744"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <!-- 一级菜单 -->
-      <el-submenu index="1">
-        <!-- 一级菜单模版 -->
-        <template slot="title">
-          <!-- 图标 -->
-          <i class="el-icon-location"></i>
-          <!-- 文本 -->
-          <span>导航一</span>
-        </template>
-        <!-- 二级菜单 -->
-        <el-menu-item index="1-4-1">
-                  <template slot="title">
-          <!-- 图标 -->
-          <i class="el-icon-location"></i>
-          <!-- 文本 -->
-          <span>二级菜单</span>
-        </template>
-          </el-menu-item>
-      </el-submenu>
-    </el-menu>
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409eff"
+          unique-opened
+          :collapse = "isCollapse"
+          :collapse-transition = "false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menulist"
+            :key="item.id"
+          >
+            <!-- 一级菜单模版 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
       </el-aside>
-      <el-main>Main</el-main>
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      //左侧菜单数据
+      menulist: [],
+      iconsObj: {
+        '125': 'el-icon-s-custom',
+        '103': 'el-icon-s-tools',
+        '101': 'el-icon-s-shop',
+        '102': 'el-icon-s-order',
+        '145': 'el-icon-s-platform'
+      },
+      //是否折叠
+      isCollapse: false,
+      //被激活的地址
+      activePath:''
+    }
+  },
+  created() {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push({ name: 'Login' })
+    },
+    async getMenuList() {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
+    //点击按钮,切换菜单的折叠与展开
+    toggleCollapse(){
+      this.isCollapse = !this.isCollapse;
+    },
+    //保存链接的激活状态
+    saveNavState(activePath){
+      window.sessionStorage.setItem('activePath',activePath);
+      this.activePath = activePath;      
     }
   }
 }
@@ -75,20 +136,25 @@ export default {
 
 .el-aside {
   background-color: #333744;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
+  .el-menu{
+    border-right: none;
+  }
 }
-
 .el-main {
   background-color: #eaedf1;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
 }
-
 body > .el-container {
   margin-bottom: 40px;
 }
 
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from "../components/Login.vue";
 import Home from "../components/Home.vue";
+import Welcome from "../components/Welcome.vue";
+import Users from "../components/user/Users.vue";
 
 Vue.use(VueRouter)
 
@@ -10,12 +12,17 @@ const routes = [
     path:'/',redirect: '/Login', 
   },
   {
-    path:'/login',component:Login,
+    path:'/login',component: Login,
     name:'Login'
   },
   {
-    path:'/home',component:Home,
-    name:'Home'
+    path:'/home',component: Home,
+    name:'Home',
+    redirect: '/welcome', 
+    children:[
+      {path:'/welcome',component:Welcome},
+      {path:'/users',component:Users},
+    ]
   },
 ]
 
